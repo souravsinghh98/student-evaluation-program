238fdc0cc5cb5b84aac638105909e24ddab8fb7b
Sourav Singh <sourav.singhh98@gmail.com>
2020-05-14 15:43:46 +0530

    postgres

--- student_eveluation_program/settings.py
+++ student_eveluation_program/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,6 +82,7 @@ WSGI_APPLICATION = 'student_eveluation_program.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -137,3 +139,4 @@ MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
