729a7da79916ce390d71b0132e2afebfa4658725
Sourav Singh <sourav.singhh98@gmail.com>
2020-05-14 15:46:27 +0530

    postgres1

--- student_eveluation_program/settings.py
+++ student_eveluation_program/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,6 +136,3 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
